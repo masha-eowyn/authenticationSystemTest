f8d7c880a2ef1a7df7425ac110d56df52956f69f
masham@rad-ace.com <masham@rad-ace.com>
2020-01-21 12:13:41 +0200

    spring boot initialization

---
+++ src/test/java/com/testExam/authenticationSystem/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testExam.authenticationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
